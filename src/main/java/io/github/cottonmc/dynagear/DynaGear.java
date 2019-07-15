394b453403e9e065ecbe42ddf0ff46f1119ee811
Meredith Espinosa <goclonefilms@gmail.com>
2019-07-14 20:29:28 -0700

    sort materials alphabetically when registering

--- src/main/java/io/github/cottonmc/dynagear/DynaGear.java
+++ src/main/java/io/github/cottonmc/dynagear/DynaGear.java
@@ -21,10 +21,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DynaGear implements ModInitializer {
 	public static final String MODID = "dynagear";
@@ -52,7 +49,9 @@ public class DynaGear implements ModInitializer {
 			Identifier id = new Identifier(MODID, mat.getName());
 			MATERIALS.putIfAbsent(id, mat);
 		}
-		for (Identifier id : MATERIALS.keySet()) {
+		List<Identifier> matIds = new ArrayList<>(MATERIALS.keySet());
+		matIds.sort(Comparator.comparing(Identifier::getPath));
+		for (Identifier id : matIds) {
 			ConfiguredMaterial mat = MATERIALS.get(id);
 			Registry.register(EQUIPMENT, new Identifier(DynaGear.MODID, mat.getName()), EquipmentSet.create(mat));
 		}
